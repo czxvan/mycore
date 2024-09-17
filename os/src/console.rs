887e9a6c2864389de113338d1a8fc7041590e801
czxvan <2889259641@qq.com>
2024-09-17 14:30:13 +0800

    feat: logging os with log crate

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,12 +1,6 @@
 use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
 
-// ANSI escape codes for colors
-pub const RESET: &str = "\x1B[0m";
-pub const INFO_COLOR: &str = "\x1B[32m"; // Green
-pub const DEBUG_COLOR: &str = "\x1B[34m"; // Blue
-pub const ERROR_COLOR: &str = "\x1B[31m"; // Red
-
 struct Stdout;
 
 impl Write for Stdout {
@@ -33,29 +27,5 @@ macro_rules! print {
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! info {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::println!("{}{}", $crate::console::INFO_COLOR, format_args!($fmt $(, $($arg)+)?));
-        $crate::print!("{}", $crate::console::RESET);
-    }
-}
-
-#[macro_export]
-macro_rules! debug {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::println!("{}{}", $crate::console::DEBUG_COLOR, format_args!($fmt $(, $($arg)+)?));
-        $crate::print!("{}", $crate::console::RESET);
-    }
-}
-
-#[macro_export]
-macro_rules! error {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::println!("{}{}", $crate::console::ERROR_COLOR, format_args!($fmt $(, $($arg)+)?));
-        $crate::print!("{}", $crate::console::RESET);
     }
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,9 +1,12 @@
 #![no_main]
 #![no_std]
 use core::arch::global_asm;
+
 mod lang_items;
 mod sbi;
+#[macro_use]
 mod console;
+mod logging;
 
 use sbi::shutdown;
 
@@ -11,12 +14,7 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 fn rust_main() {
-    // println!("Hello, world!");
-    
     clear_bss();
-    
-    println!("hello, world!");
-    println!("你好，世界！");
     
     print_memory_layout();
     // panic!("czxcx");
@@ -36,11 +34,27 @@ fn print_memory_layout() {
         fn boot_stack_lower_bound();
         fn boot_stack_top();
     }
-    info!(".text\t[{:#x}, {:#x})", stext as usize, etext as usize);
-    info!(".rodata\t[{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data\t[{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(".bss\t[{:#x}, {:#x})", sbss as usize, ebss as usize);
-    debug!(".stack\t[{:#x}, {:#x})", boot_stack_lower_bound as usize, boot_stack_top as usize);
+    logging::init();
+    println!("[kernel] Hello, world!");
+    log::trace!(
+        "[kernel] .text  \t[{:#x}, {:#x})",
+        stext as usize, etext as usize
+    );
+    log::debug!(
+        "[kernel] .rodata\t[{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    log::info!(
+        "[kernel] .data  \t[{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    log::warn!(
+        "[kernel] .stack \t[{:#x}, {:#x})",
+        boot_stack_lower_bound as usize, boot_stack_top as usize
+    );
+    log::error!("[kernel] .bss   \t[{:#x}, {:#x})",
+        sbss as usize, ebss as usize
+    );
 }
 
 fn clear_bss() {
